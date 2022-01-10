d3718b5cfcde8cac770f02f7fbfa4df45e6bbe9c
AnthonyYao7 <anthonyjyao@gmail.com>
2022-01-10 13:26:34 -0500

    work

--- nn.py
+++ nn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from os import walk
+import time
 
 """
 Steps:
@@ -52,22 +53,140 @@ def split_input_target(sequence):
     return input_text, target_text
 
 
-def process_single_episode(tokens, chars_from_ids):
-    dataset = tf.data.Dataset.from_tensor_slices(tokens)
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return pos * angle_rates
 
-    examples_per_epoch = tokens.shape[0]
+# not using transformer yet
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
 
-    sequences = dataset.batch(seq_length + 1, drop_remainder=True)
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
 
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class Model(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init__(self)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(rnn_units,
+                                       return_sequences=True,
+                                       return_state=True)
+        self.dense = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = inputs
+        x = self.embedding(x, training=training)
+        if states is None:
+            states = self.gru.get_initial_state(x)
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.dense(x, training=training)
+
+        if return_state:
+            return x, states
+        else:
+            return x
+
+
+class OneStep(tf.keras.Model):
+    def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+        super().__init__()
+        self.temperature = temperature
+        self.model = model
+        self.chars_from_ids = chars_from_ids
+        self.ids_from_chars = ids_from_chars
+
+        # Create a mask to prevent "[UNK]" from being generated.
+        skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+        sparse_mask = tf.SparseTensor(
+            # Put a -inf at each bad index.
+            values=[-float('inf')]*len(skip_ids),
+            indices=skip_ids,
+            # Match the shape to the vocabulary
+            dense_shape=[len(ids_from_chars.get_vocabulary())])
+        self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+    @tf.function
+    def generate_one_step(self, inputs, states=None):
+        # Convert strings to token IDs.
+        input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+        input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+        # Run the model.
+        # predicted_logits.shape is [batch, char, next_char_logits]
+        predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                              return_state=True)
+        # Only use the last prediction.
+        predicted_logits = predicted_logits[:, -1, :]
+        predicted_logits = predicted_logits/self.temperature
+        # Apply the prediction mask: prevent "[UNK]" from being generated.
+        predicted_logits = predicted_logits + self.prediction_mask
+
+        # Sample the output logits to generate token IDs.
+        predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+        predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+        # Convert from token ids to characters
+        predicted_chars = self.chars_from_ids(predicted_ids)
+
+        # Return the characters and model state.
+        return predicted_chars, states
 
 
 def main():
-    tokens, token_table, char_table = read_transcripts()
+    tokens, ids_from_chars, chars_from_ids = read_transcripts()
 
-    for i in range(tokens.shape[0]):
-        process_single_episode(tokens[i], char_table)
-        exit(69)
+    dataset = tf.data.Dataset.from_tensor_slices(tokens)
 
+    dataset = dataset.map(split_input_target)
+
+    # for example, target in dataset.as_numpy_iterator():
+    #     print("Input: ", len(text_from_ids(example, chars_from_ids).numpy()) + 1)
+    #     print("Output: ", text_from_ids(target, chars_from_ids).numpy())
+
+    BATCH_SIZE = 1
+    BUFFER_SIZE = 10
+
+    dataset = (
+        dataset
+        .shuffle(BUFFER_SIZE)
+        .batch(BATCH_SIZE, drop_remainder=True)
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+
+    model = Model(
+        vocab_size=len(ids_from_chars.get_vocabulary()),
+        embedding_dim=256,
+        rnn_units=1024
+    )
+
+    loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='adam', loss=loss)
+
+    history = model.fit(dataset, epochs=10)
+
+    one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+
+    start = time.time()
+    states = None
+    next_char = tf.constant(['ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:'])
+    result = [next_char]
+
+    for n in range(1000):
+        next_char, states = one_step_model.generate_one_step(next_char, states=states)
+        result.append(next_char)
+
+    result = tf.strings.join(result)
+    end = time.time()
+    print(result, '\n\n' + '_' * 80)
+    print('\nRun time:', end - start)
 
 
 if __name__ == "__main__":
